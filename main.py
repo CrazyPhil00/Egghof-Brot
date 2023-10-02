dd897854600af67e269242ca68c78ab121a29245
Phil <philippsiebenforcher@gmail.com>
2023-10-02 18:35:48 +0200

    added gitignore

--- main.py
+++ main.py
@@ -1,139 +1,123 @@
 import datetime
-import smtplib
-import ssl
-from email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-from email.mime.text import MIMEText
-
 import os
 import json
-import pandas as pd
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import Alignment
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from credentials import password, email
 
+# Read the data.json file to get product names and prices
+with open('data.json', 'r') as data_file:
+    products = json.load(data_file)
 
-def sendMail():
-    email_sender = 'philippsiebenforcher@gmail.com'
-    email_password = ''
-    email_receiver = ''
+# Create a dictionary to store product prices
+product_prices = {product['name']: product['price'] for product in products}
 
-    subject = "Brotbestellung für den " + str(datetime.date.today())
-    body = """
-    """
+# Define the directory containing apartment JSON files
+directory = 'orders/'
 
-    # Create a multipart email message
-    em = MIMEMultipart()
-    em['from'] = email_sender
-    em['to'] = email_receiver
-    em['subject'] = subject
+# Initialize a dictionary to store product details for all orders
+all_products_details = {}
 
-    # Add text/plain part (the message body)
-    em.attach(MIMEText(body, 'plain'))
+# Initialize variables to store apartment orders and summary HTML
+apartment_orders_html = ''
+summary_html = '<h2>Gesamte Bestellung</h2>\n'
+summary_html += '<table border="1">\n'
+summary_html += '<tr><th>Product</th><th>Amount</th><th>Total Price</th></tr>\n'
 
-    # Attach a file (e.g., attachment.txt)
-    attachment_path = 'apartment_orders.xlsx'
-    attachment_name = 'apartment_orders.xlsx'
+# Get a list of JSON files in the directory
+json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
 
-    with open(attachment_path, 'rb') as attachment_file:
-        # Create a base MIME type for the attachment
-        attachment = MIMEBase('application', 'octet-stream')
-        attachment.set_payload(attachment_file.read())
+# Iterate through JSON files in the directory
+for filename in json_files:
+    apartment_number = os.path.splitext(filename)[0]
+    with open(os.path.join(directory, filename), 'r') as apartment_file:
+        apartment_order = json.load(apartment_file)
+        second_name = apartment_order['name']
+        order_items = apartment_order['order']
 
-    # Encode the attachment in ASCII
-    encoders.encode_base64(attachment)
+        # Initialize a dictionary to store product details for the apartment
+        product_details = {}
 
-    # Set the filename and add the attachment to the message
-    attachment.add_header('Content-Disposition', f'attachment; filename={attachment_name}')
-    em.attach(attachment)
+        # Collect product details for the apartment
+        for item in order_items:
+            product_id = item['id']
+            amount = item['amount']
+            product_name = next((p['name'] for p in products if p['id'] == product_id), '')
+            product_price = product_prices.get(product_name, 0)
+            total_price = amount * product_price
+            product_details[product_name] = {
+                'Amount': amount,
+                'Total Price': total_price
+            }
 
-    context = ssl.create_default_context()
+        # Collect product details for the summary
+        for product_name, details in product_details.items():
+            if product_name not in all_products_details:
+                all_products_details[product_name] = {
+                    'Amount': details['Amount'],
+                    'Total Price': details['Total Price']
+                }
+            else:
+                all_products_details[product_name]['Amount'] += details['Amount']
+                all_products_details[product_name]['Total Price'] += details['Total Price']
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        # Create an HTML table for the individual apartment orders
+        apartment_orders_html += f'<h2>[Apartment-{apartment_number}] {second_name}</h2>\n'
+        apartment_orders_html += '<table border="1">\n'
+        apartment_orders_html += '<tr><th>Bestellung</th><th>Anzahl</th><th>Gesamter Preis</th></tr>\n'
 
+        for product_name, details in product_details.items():
+            amount = details['Amount']
+            total_price = details['Total Price']
+            apartment_orders_html += (
+                f'<tr><td>{product_name}</td><td>{amount}</td><td>{total_price:.2f}€</td></tr>\n'
+            )
 
-def createExcel():
-    # Read the data.json file to get product names and prices
-    with open('data.json', 'r', encoding='utf-8') as data_file:
-        products = json.load(data_file)
+        apartment_orders_html += '</table>\n'
 
-    # Create a dictionary to store product prices
-    product_prices = {product['name']: product['price'] for product in products}
+# Create an HTML table for the summary of all ordered products
+for product_name, details in all_products_details.items():
+    amount = details['Amount']
+    total_price = details['Total Price']
+    summary_html += (
+        f'<tr><td>{product_name}</td><td>{amount}</td><td>{total_price:.2f}€</td></tr>\n'
+    )
 
-    # Create a list to store apartment data
-    apartment_data = []
+summary_html += '</table>\n'
 
-    # Define the directory containing apartment JSON files
-    directory = 'checkout/orders/'
+# Create the HTML content
+html_content = f'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Apartment Orders</title>
+</head>
+<body>
+    {apartment_orders_html}
+    {summary_html}
+</body>
+</html>
+'''
 
-    # Iterate through JSON files in the directory
-    for filename in os.listdir(directory):
-        if filename.endswith('.json'):
-            apartment_number = os.path.splitext(filename)[0]
-            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as apartment_file:
-                apartment_order = json.load(apartment_file)
-                second_name = apartment_order['name']
-                order_items = apartment_order['order']
+# Define your email configuration
+email_sender = email
+email_password = password
+email_receiver = 'philippsiebenforcher@gmail.com'
 
-                # Create a dictionary to store the apartment's order
-                apartment_order_data = {'Appartment': apartment_number, 'Nachname': second_name}
+subject = "Brotbestellung für den " + str(datetime.date.today())
 
-                # Add product amounts to the dictionary
-                for item in order_items:
-                    product_id = item['id']
-                    amount = item['amount']
-                    product_name = next((p['name'] for p in products if p['id'] == product_id), '')
+em = MIMEMultipart()
+em['from'] = email_sender
+em['to'] = email_receiver
+em['subject'] = subject
 
-                    apartment_order_data[product_name] = amount
+# Change 'plain' to 'html' to send the email as HTML
+em.attach(MIMEText(html_content, 'html'))
 
-                total_price = 0
-                for item in order_items:
-                    product_id = item['id']
-                    amount = item['amount']
-                    product_name = next((p['name'] for p in products if p['id'] == product_id), '')
-                    product_price = product_prices.get(product_name, 0)
-                    total_price += amount * product_price
+context = ssl.create_default_context()
 
-                apartment_order_data['Preis'] = f"{total_price:0.2F}€".replace(",",
-                                                                               ".")  # Add the total price to the dictionary
-
-                apartment_data.append(apartment_order_data)
-
-    # Create a DataFrame from the apartment data
-    df = pd.DataFrame(apartment_data)
-
-    # Reorder the columns to have 'Preis' as the last column
-    column_order = [col for col in df.columns if col != 'Preis'] + ['Preis']
-    df = df[column_order]
-
-    # Save the DataFrame to an Excel file
-    excel_file = 'apartment_orders.xlsx'
-    df.to_excel(excel_file, index=False, engine='openpyxl')
-
-    # Load the workbook
-    workbook = load_workbook(excel_file)
-    worksheet = workbook.active
-
-    # Adjust column widths to fit the content
-    for column in worksheet.columns:
-        max_length = 0
-        column = [cell for cell in column]
-        for cell in column:
-            try:
-                if len(str(cell.value)) > max_length:
-                    max_length = len(cell.value)
-            except:
-                pass
-        adjusted_width = (max_length + 2) * 1.2  # Adjust this value as needed
-        worksheet.column_dimensions[get_column_letter(column[0].column)].width = adjusted_width
-
-    # Save the modified workbook
-    workbook.save(excel_file)
-
-
-createExcel()
-sendMail()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
